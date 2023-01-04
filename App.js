6919629723bd9da1cc50fcc0c78c444a58cf4506
maxmay94 <maxmay94@gmail.com>
2023-01-04 17:06:54 -0500

    learn about stylesheets

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import { StyleSheet, Text, View, Button } from 'react-native'
 export default function App() {
   return (
     <View style={styles.container}>
-      <View>
+      <View style={styles.dummyText}>
         <Text>Another piece of text!</Text>
       </View>
-      <Text>Hello World!</Text>
-      <Button title="Tap Me!" />
+      <Text style={styles.dummyText}>
+        Hello World!!
+      </Text>
+      <Button title="Tap Me!"/>
     </View>
   )
 }
@@ -18,5 +20,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  dummyText: {
+    margin: 16, 
+    padding: 16,
+    borderWidth: 2, 
+    borderColor: 'blue', 
   },
 })

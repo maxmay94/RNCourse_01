67e4325021d0095211022f647667aa40cfb75cc0
maxmay94 <maxmay94@gmail.com>
2023-01-05 11:21:06 -0500

    learn scroll view

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native'
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('')
@@ -27,15 +27,17 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        {
-          courseGoals.map((goal) => 
-            <View style={styles.goalItem} key={goal}>
-              <Text style={styles.goalText}>
-                  {goal}
-              </Text>
-            </View>
-          )
-        }
+        <ScrollView alwaysBounceVertical={false} >
+          {
+            courseGoals.map((goal) => 
+              <View style={styles.goalItem} key={goal}>
+                <Text style={styles.goalText}>
+                    {goal}
+                </Text>
+              </View>
+            )
+          }
+        </ScrollView>
       </View>
     </View>
   )

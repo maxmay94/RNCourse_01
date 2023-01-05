71ade515edf88ee75d81bf311ec7e2f1fb317be7
maxmay94 <maxmay94@gmail.com>
2023-01-05 12:09:56 -0500

    delete item working

--- App.js
+++ App.js
@@ -13,8 +13,10 @@ export default function App() {
     ])
   }
 
-  function deleteGoalHandler() {
-    console.log('DELETE')
+  function deleteGoalHandler(id) {
+    setCourseGoals(currentCourseGoals => {
+      return currentCourseGoals.filter((goal) => goal.id !== id)
+    })
   }
 
   return (
@@ -24,7 +26,13 @@ export default function App() {
         <FlatList 
           data={courseGoals} 
           renderItem={(itemData) => {
-            return <GoalItem text={itemData.item.text} onDeleteItem={deleteGoalHandler}/>
+            return ( 
+              <GoalItem 
+                text={itemData.item.text} 
+                id={itemData.item.id} 
+                onDeleteItem={deleteGoalHandler}
+              />
+            )
           }} 
           keyExtractor={(item, index) => { return item.id }}
           alwaysBounceVertical={false} />
